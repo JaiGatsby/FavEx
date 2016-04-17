73a31edb0b9b7392617ca8d9d26aa51d4a13a6ec
Vikram Sambamurthy <Vikram Sambamurthy>
2016-04-17 08:21:00 +0800

    aaa

--- www/js/lolz.js
+++ www/js/lolz.js
@@ -240,3 +240,39 @@ angular.module('ionicApp', ['ionic'])
 	console.log($scope.data.qty);
 	}
 })
+.controller('AppCtrl', function($scope)
+{
+  $scope.data = {
+	  itsc: "",
+	  pword: ""
+  };
+  $scope.btxt = "Go!";
+  $scope.test = $scope.itsc;
+  $scope.check = function(){
+	  
+	  alert("Checking database");
+      $state.go('menu.tabs.home');
+      
+      
+	  /*if(valid){
+		if(!userexists){
+			
+		}
+		login		
+	  }
+	  else{
+		  
+	  }*/
+  };
+  
+})
+.filter('yesNo', function() {
+    return function(input) {
+        if(input == true){
+			document.getElementById("b").setAttribute("disabled", "");
+		}else{
+			document.getElementById("b").removeAttribute("disabled");
+		}
+		return "";
+    }
+});
